958049b1c6c7f8ca9ce77800ff01ef78cf38b693
aluno.lab <aluno.lab@ides.lab>
2024-09-24 17:15:05 -0300

    up

--- script.js
+++ script.js
@@ -2,22 +2,27 @@
 const descricao = document.getElementById("descricao");
 
 function escrever(texto){
-    const textoArmazenar = texto.innerHTML.split(""); // Separar os elementos com o método split
-    texto.innerHTML = ""; // Utilizado para ocultar o texto capturado no HTML
+    const textoArmazenar = texto.textContent.split(""); // Separar os elementos com o método split
+    texto.textContent = ""; // Utilizado para ocultar o texto capturado no HTML
 
     function digitar(indiceInicial){
+        
         if(indiceInicial < textoArmazenar.length){
-            texto.innerHTML += textoArmazenar[indiceInicial];
+            texto.textContent += textoArmazenar[indiceInicial];
+            
             setTimeout(() =>
                 digitar (indiceInicial + 1),75
             )
         }else{
             setTimeout(() => {
                 escrever(texto); // Chama a função novamente
-            },5000)
+            },4000)
         }
     }
 
     digitar(0); // Começar no índice 0
 }
 escrever(descricao)
+
+
+
