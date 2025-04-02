b0cd6a155f817f5544b15880ddc14426393133ff
Evy Mih <evellynkoster@gmail.com>
2025-04-02 15:36:19 -0300

    style

--- script.js
+++ script.js
@@ -46,17 +46,7 @@ setTimeout(() => {
     escreverNome(nome)
 },1200)
 
-//função expandir habilidades 
-let habilidades = document.getElementById("hab")
-let contatos = document.getElementById("ctt")
 
-habilidades.addEventListener('mouseover', () => {
-    habilidades.style.transition 
-});
-  
-habilidades.addEventListener('mouseout', () => {
-    habilidades.style.transition 
-});
 
 
 
